f7c2b27ed6ed30478a8e18f7a9af04afee4f4764
aimansae <saeedaimann@gmail.com>
2025-07-29 19:42:15 +0200

    Final commit

--- app/components/PostSection.tsx
+++ app/components/PostSection.tsx
@@ -14,20 +14,20 @@ const PostSection = async () => {
   return (
     <div className="pb-16 flex flex-col  sm:gap-24">
       <div id="blog">
-        <h2 className="mb-6 sm:mb-16 font-bold text-white text-xl sm:text-3xl ">
+        <h2 className="sm:mb-16 font-bold text-white text-xl sm:text-3xl ">
           Latest from the blog
         </h2>
       </div>
 
-      <div className=" grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="mb-4 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {posts.map((post) => (
           <Link
             key={post.id}
             href={`/posts/${post.id}`}
-            className="bg-yellow-200 group flex flex-col justify-between  rounded-xl border border-gray-800   hover:bg-gray-600/30 transition-colors p-4 sm:p-6 "
+            className="  group gap-2 flex flex-col justify-between  rounded-xl border border-gray-800   hover:bg-gray-600/30 transition-colors p-4 sm:p-6 "
           >
             <article className="flex">
-              <h3 className="text-md sm:text-xl font-semibold leading-snug tracking-tight text-white group-hover:text-purple-400 transition-colors">
+              <h3 className=" sm:text-xl font-semibold leading-snug tracking-tight text-white group-hover:text-purple-400 transition-colors">
                 {post.title}
               </h3>
             </article>
